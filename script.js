0083cbfeb136d30ce71f5b98b72ba265e579b842
ING-ErikapCamberosR <41335323+erikapcamberosr@users.noreply.github.com>
2023-05-31 12:02:19 -0600

    prueba_1
    prueba uno

---
+++ script.js
@@ -0,0 +1,6 @@
+const Button_FotoEvidencia = document.getElementById("btn_iniciarReporte_1");
+
+Button_FotoEvidencia.onclick = function () {
+    document.location.href = "fotos_evidencia.html";
+    //cargar_Juego();
+}
